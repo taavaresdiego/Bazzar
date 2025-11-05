cb5cca731b822d461154f78e7504df8271324603
Diego Santos Tavares <taavares.diego@gmail.com>
2025-11-05 15:34:14 -0300

    feat: Continua desenvolvimento do backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import { Mongo } from "./database/mongodb.js";
 import { config } from "dotenv";
-import authRouter from "./auth/auth.js";
+import { setupAuthRoutes } from "./auth/auth.js";
 
 config();
 
@@ -16,10 +16,19 @@ async function main() {
     mongoConnectionString: process.env.MONGO_CS,
     mongoDbName: process.env.MONGO_DB_NAME,
   });
+
   console.log(mongoConnection);
+
+  if (mongoConnection.error) {
+    console.error("Falha ao conectar ao MongoDB. Encerrando servidor.");
+    process.exit(1);
+  }
+
   app.use(express.json());
   app.use(cors());
 
+  const authRouter = setupAuthRoutes(app);
+  app.use("/auth", authRouter);
   app.get("/", (req, res) => {
     res.send({
       sucess: true,
@@ -27,8 +36,6 @@ async function main() {
       body: "Bem-vindo ao Supermercado",
     });
   });
-
-  app.use("/auth", authRouter);
 
   app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
